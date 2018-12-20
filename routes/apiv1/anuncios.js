3ba04950c1d1f9859684d35261c59c11df6c871e
Dani <Daniel.rica86@gmail.com>
2018-12-20 20:04:50 +0100

    Filtros anuncios

--- routes/apiv1/anuncios.js
+++ routes/apiv1/anuncios.js
@@ -13,8 +13,48 @@ const Anuncio = require('../../models/Anuncio');
  * obtener una lista de anuncios
  */
 
- router.get('/', (req,res,next) => {
-     Anuncio.find().exec((err, lista) => {
+router.get('/', (req,res,next) => {
+
+    //hay que poner indices para que vaya mejor en el modelo
+    //Datos de entrada
+    const nombre = req.query.nombre;
+    const venta = req.query.venta;
+    const precio = req.query.precio;
+    const limit = parseInt(req.query.limit);
+    const skip = parseInt(req.query.skip); // para saltarse
+    const fields = req.query.fields;
+    const sort = req.query.sort;
+
+
+
+
+    const filter = {};
+
+
+    //busqueda por nombre
+    if(nombre) {
+        filter.nombre = nombre;
+    }
+
+    //busqueda si se venden o buscan
+    if(venta) {
+        filter.venta = venta
+    }
+
+     //busqueda por precio
+     if(precio) {
+        filter.precio = precio
+    }
+
+    const query = Anuncio.find(filter);
+
+    query.limit(limit);
+    query.skip(skip);
+    query.select(fields);
+    query.sort(sort);
+
+
+     query.exec((err, lista) => {
 
         if(err){
             next (err);
@@ -22,11 +62,7 @@ const Anuncio = require('../../models/Anuncio');
         }
 
         res.json({ success: true, results: lista});
-        console.log(lista);
      });
-
-
-   
 
  });
 
